1a12d52d504c9b18e82b63360e155064d83f691a
Carl Englund <englund92@gmail.com>
2015-02-15 15:08:41 +0100

    Initial commit, added opengl bootstrap

---
+++ src/main.cpp
@@ -0,0 +1,73 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW
+#ifdef __APPLE_CC__
+#include <OpenGL/gl3.h>
+#define GLFW_INCLUDE_GLCOREARB
+#else 
+#include <GL/glew.h>
+#endif
+
+#define GLFW_INCLUDE_GL3
+#define GLFW_NO_GLU
+// Include GLFW
+#include <GLFW/glfw3.h>
+
+// Include GLM
+#include <glm/glm.hpp>
+#include <iostream>
+
+#include <common/Init.h>
+#include <common/Shader.h>
+
+using namespace glm;
+
+int main( void )
+{
+  
+  //Create init object
+  Init init = Init();
+
+  //Initialize glfw
+  init.glfw(4,1);
+
+  //Open a window
+  GLFWwindow* window = init.window();
+  glfwSetWindowTitle(window, "Ducky Water");
+  //Print window info
+  init.printWindowInfo(window);
+
+  //Make opened window current context
+  glfwMakeContextCurrent(window);
+
+  
+  init.glew();
+
+  // Dark blue background
+  glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+  //Load in shaders
+  static ShaderProgram prog("../vertShader.vert", "../fragShader.frag");
+   
+  do{
+
+
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    glClear(GL_COLOR_BUFFER_BIT);
+    prog();
+
+   
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  } // Check if the ESC key was pressed or the window was closed
+  while( !glfwWindowShouldClose(window) );
+
+  // Close OpenGL window and terminate GLFW
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  
+}
---
+++ vertShader.vert
@@ -0,0 +1,8 @@
+#version 400 core
+in vec3 inputColor;
+in vec4 inputPosition;
+out vec3 color;
+void main(void) {
+  color = abs(inputPosition.xyz);
+  gl_Position = inputPosition;
+}
