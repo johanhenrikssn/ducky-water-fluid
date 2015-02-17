e379b3e2ce19b02ec3b8f271eda8cebff424123e
Carl Englund <englund92@gmail.com>
2015-02-17 10:10:34 +0100

    Fixed some particlez

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -2,38 +2,8 @@
 //  Particle.cpp
 //  Ducky-Water
 //
-//  Created by Mattias Palmgren on 2015-02-15.
+//  Created by Carl Englund on 16/02/15.
 //
 //
 
 #include "Particle.h"
-
-using namespace glm;
-
-Particle::Particle(){
-    position = vec3(0,0,0);
-    velocity = vec3(0,0,0);
-}
-
-
-Particle::Particle(vec3 p){
-    position = p;
-    velocity = vec3(0,0,0);
-}
-
-void Particle::setPosition(vec3 p) {
-    position = p;
-}
-
-vec3 Particle::getPosition() {
-    
-    return position;
-}
-
-void Particle::setVelocity(vec3 v){
-    velocity = v;
-}
-
-vec3 Particle::getVelocity(){
-    return velocity;
-}
--- src/Particle.h
+++ src/Particle.h
@@ -2,7 +2,7 @@
 //  Particle.h
 //  Ducky-Water
 //
-//  Created by Mattias Palmgren on 2015-02-15.
+//  Created by Carl Englund on 16/02/15.
 //
 //
 
@@ -11,27 +11,4 @@
 
 #include <stdio.h>
 
-// Include GLM
-#include <glm/glm.hpp>
-
-using namespace glm;
-
-
 #endif /* defined(__Ducky_Water__Particle__) */
-
-class Particle {
-    
-    public:
-        Particle();
-        Particle(vec3 p);
-    
-        void setPosition(vec3 p);
-        vec3 getPosition();
-        void setVelocity(vec3 v);
-        vec3 getVelocity();
-    
-    protected:
-        vec3 position;
-        vec3 velocity;
-    
-};
